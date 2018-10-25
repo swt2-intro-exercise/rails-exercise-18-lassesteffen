0375f2ab9159c5ec669da1ae698c0c99cf987b37
lassesteffen <lasse.steffen@t-online.de>
2018-10-25 13:43:34 +0200

    added spec for new author fields

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
- 
- it "should render withour error" do
-   visit new_author_path
- end
+
+  it "should render withour error" do
+    visit new_author_path
+  end
+
+  it "should have input fields for first_name, last_name and homepage" do
+    visit new_author_path
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
