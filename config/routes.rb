6456eb735b2f0fa3b422884f498e11ba074be103
lassesteffen <lasse.steffen@t-online.de>
2018-10-25 13:00:10 +0200

    new author page added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  
+  resources :author
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
