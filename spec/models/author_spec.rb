84a25d39674bb1dcfc3e460d1937b8f4c1fe4872
lassesteffen <lasse.steffen@t-online.de>
2018-10-26 14:08:22 +0200

    validated that user last and first name is present

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Author, type: :model do
+
   let(:first_name) { 'Alan' }
   let(:last_name) { 'Turing' }
   let(:homepage) { 'http://wikipedia.org/Alan_Turing' }
@@ -9,13 +10,24 @@ describe Author, type: :model do
     @author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
   end
 
-  it 'should create a new instance of type' do
-    expect(@author.first_name).to eq(first_name)
-    expect(@author.last_name).to eq(last_name)
-    expect(@author.homepage).to eq(homepage)
+
+  context 'with all needed fields specified' do
+    it 'should create a new instance of type' do
+      expect(@author.first_name).to eq(first_name)
+      expect(@author.last_name).to eq(last_name)
+      expect(@author.homepage).to eq(homepage)
+    end
+
+    it 'should have a property name which combines first and last name' do
+      expect(@author.name).to eq('Alan Turing')
+    end
   end
 
-  it 'should have a property name which combines first and last name' do
-    expect(@author.name).to eq('Alan Turing')
+  context 'without a last name' do
+    let(:last_name) { '' }
+
+    it 'should not validate' do
+      expect(@author).to_not be_valid
+    end
   end
 end
