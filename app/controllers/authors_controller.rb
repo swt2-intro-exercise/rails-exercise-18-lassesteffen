8bf7799be387cb0e05a92f6237b6a73ac287f83b
lassesteffen <lasse.steffen@t-online.de>
2018-10-26 08:43:47 +0200

    renaming to author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @authors = Author.find(params[:id])
+    @author = Author.find(params[:id])
   end
 
   def new
@@ -15,6 +15,6 @@ class AuthorsController < ApplicationController
   private
 
   def author_params
-    params.require(:authors).permit(:first_name, :last_name, :homepage)
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
