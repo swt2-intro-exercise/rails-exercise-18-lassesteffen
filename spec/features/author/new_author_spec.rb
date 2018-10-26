df470c52ce6395fca80e838dfe37513820ddd060
lassesteffen <lasse.steffen@t-online.de>
2018-10-26 08:41:02 +0200

    Working version for test 4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
+describe "New authors page", type: :feature do
 
   it "should render withour error" do
     visit new_author_path
@@ -8,16 +8,16 @@ describe "New author page", type: :feature do
 
   it "should have input fields for first_name, last_name and homepage" do
     visit new_author_path
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
+    expect(page).to have_field('authors[first_name]')
+    expect(page).to have_field('authors[last_name]')
+    expect(page).to have_field('authors[homepage]')
   end
 
   it "should create a new user when the form is filled" do
     visit new_author_path
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[last_name]', with: 'Turing'
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    page.fill_in 'authors[first_name]', with: 'Alan'
+    page.fill_in 'authors[last_name]', with: 'Turing'
+    page.fill_in 'authors[homepage]', with: 'http://wikipedia.org/Alan_Turing'
     find('input[type="submit"]').click
   end
 end
