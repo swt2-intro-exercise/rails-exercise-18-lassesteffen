2fb87b4fc484a832c5c994d7e67f4ce586fc3650
lassesteffen <lasse.steffen@t-online.de>
2018-10-28 20:46:12 +0100

    Added has_and_belongs_to_many relationship between author and paper

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be initialized with a list of authors' do
+    @paper = FactoryBot.create :paper
+    @author = FactoryBot.build :author
+    expect(@paper.authors.size).to eq(1)
+    expect(@paper.authors.first.first_name).to eq(@author.first_name)
+  end
 end
